a949de557fcba2f279616dab073467f48c79db86
Nagul75 <nagulk697@gmail.com>
2026-05-17 18:34:29 +0530

    feat: add remote tf state in S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,17 @@ terraform {
       version = "~> 5.0"
     }
   }
+  
+  backend "s3" {
+    bucket       = "devops-assignment-tfstate-regal"
+    key          = "devops-assignment/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
+
+
 
 provider "aws" {
   region = "ap-south-1"
